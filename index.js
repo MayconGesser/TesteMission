b78ae3c93605791dc77c54e82e92423131bc0eda
Maycon <maycon.antonio.gesser@gmail.com>
2019-04-24 19:14:12 -0300

    Modulariza download de imagens e cria diretório para recebê-las

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const http = require('http')
 const https = require('https')
 const fs = require('fs')
+//const JSZip = require('jszip')
+const path = require('path')
 const port = 3000
 
 var JSON_imagens;
@@ -19,19 +21,26 @@ function extrairNomeImagem(url_imagem){
 	return reg.exec(url_imagem)[0];
 }
 
-const requestHandler = (request, response) => {
+function baixarImagens(){
 	//fonte: https://stackoverflow.com/questions/11944932/how-to-download-a-file-with-node-js-without-using-third-party-libraries
 	//fonte: https://stackoverflow.com/questions/18310990/nodejs-lost-data-when-using-loop-for-download-many-files
+	var dir = 'images';
 	for(var i in JSON_imagens.images){
 		(function(i){
 			var nome_imagem = extrairNomeImagem(JSON_imagens.images[i]);
-			var imagem = fs.createWriteStream(nome_imagem);
+			//fonte: https://www.w3schools.com/nodejs/met_path_join.asp
+			var imagem = fs.createWriteStream(path.join(dir,nome_imagem));
 			var request = https.get(JSON_imagens.images[i],function(resposta){
 				resposta.pipe(imagem);
 			});
 			request.end();
 		})(i);
 	}
+}
+
+const requestHandler = (request, response) => {
+	//cria diretorio assincronizadamente
+	fs.mkdir('images', baixarImagens);
   response.end(JSON.stringify(JSON_imagens));
 }
 
